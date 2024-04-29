7f8adeb515ca2ae1a9ffe475f6e5f645d291d86a
Munshi Muntasir Rifat <rifat2002m@gmail.com>
2024-04-29 15:02:17 +0600

    simple validation

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,27 +1,64 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, GithubAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, GithubAuthProvider, signInWithEmailAndPassword } from "firebase/auth";
 import app from "../../firebase.init";
 import { FaGithub } from "react-icons/fa";
 import { FaGoogle } from "react-icons/fa6";
 import { useState } from "react";
+import { FaRegEye, FaEyeSlash } from "react-icons/fa";
 
 const Login = () => {
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
     const gitProvider = new GithubAuthProvider();
-    const [googleUser, setGoogleUser] = useState(null)
+    const [googleUser, setGoogleUser] = useState(null);
+    const [userError, setUserError] = useState('');
+    const [show, setShow] = useState(false);
+
+    //Log In
+    const handleLogIn = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+        setUserError('');
+        if (password.length < 6) {
+            setUserError("Password should be at least 6 character");
+            return;
+        }
+        // else if (password === "password") {
+        //     setUserError('Correct');
+        //     return;
+        // }
+        // else if(password !== "password") {
+        //     setUserError("Enter Correct Password");
+        //     return;
+        // }
+        
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result);
+                setUserError('');
+            })
+            .catch(error => {
+                console.error(error.message);
+            })
+
+    }
+
 
     //Google Sign In
     const handleSign = () => {
         signInWithPopup(auth, provider)
             .then(result => {
                 const user = result.user;
-                setGoogleUser(user)
+                setGoogleUser(user);
                 console.log(user);
             })
             .catch((error) => {
                 console.log('Google Error', error.message);
             })
     }
+
     //Github
     const handleGithub = () => {
         signInWithPopup(auth, gitProvider)
@@ -46,6 +83,7 @@ const Login = () => {
                 console.log('error happen', error.message);
             })
     }
+
     return (
         <div>
             {googleUser &&
@@ -60,21 +98,25 @@ const Login = () => {
                     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <h1 className="text-5xl font-bold mx-auto p-4">Login now!</h1>
 
-                        <form className="card-body">
+                        <form onSubmit={handleLogIn} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="email" name="email" placeholder="Enter Email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
+                                <p className="flex space-y-2">
+                                    <input type={show ? "text" : "password"} name="password" placeholder="password" className="input input-bordered mr-4" required />
+                                    <span onClick={() => setShow(!show)} >
+                                        <p className="text-2xl">
+                                            {
+                                                show ? <FaEyeSlash /> : <FaRegEye />
+                                            }
+                                        </p>
+
+                                    </span>
+                                </p>
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-error text-white">Login</button>
@@ -91,6 +133,11 @@ const Login = () => {
 
                             </div>
                         </form>
+                        <p className="text-red-600 mx-auto mb-4 font-semibold">
+                            {
+                                userError && <> {userError}</>
+                            }
+                        </p>
                     </div>
                 </div>
             </div>
