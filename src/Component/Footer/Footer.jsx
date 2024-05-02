41df626747c30fe0dafbef09456a56a3aa6d094e
Munshi Muntasir Rifat <rifat2002m@gmail.com>
2024-05-02 23:03:59 +0600

    footer

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,8 +1,33 @@
+import { IoFastFoodOutline } from "react-icons/io5";
 
 const Footer = () => {
     return (
         <div>
-            foot
+            <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+                <div className="text-2xl text-blue-800 font-bold italic">
+                    <p >Authentic Best Foods in The Town</p>
+                    <p className="text-red-600 flex gap-3"> It's Yammy <IoFastFoodOutline />
+                    </p>
+                </div>
+                <div className="font-semibold">
+                    <p className="flex gap-4">
+                        <p className="underline">Dhaka</p>
+                        <p className="underline">Sylhet</p>
+                        <p className="underline">Rsjshahi</p>
+                        <p className="underline">Khulna</p>
+                    </p>
+                </div>
+                <div>
+                    <p> Head Office: House #29, Road #02, Sector #11</p>
+                    <p>Uttara, Dhaka, Bangladesh</p>
+                    <p>Phone: +8801987654321</p>
+                </div>
+
+
+                <aside>
+                    <p>Copyright © 2024 - All right reserved by Food Court Ltd</p>
+                </aside>
+            </footer>
         </div>
     );
 };
